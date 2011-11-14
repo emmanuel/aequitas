e8554a232df1cfc27d400c6a8908ff68471bc931
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-11-14 01:38:42 -0800

    Add a rudimentary .rvmrc configuration.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+require 'dm-core/spec/setup'
+require 'dm-core/spec/lib/adapter_helpers'
+
+require 'dm-validations'
+require 'dm-types'
+require 'dm-migrations'
+
+class Hash
+  def except(*keys)
+    hash = dup
+    keys.each { |key| hash.delete(key) }
+    hash
+  end
+end
+
+SPEC_ROOT = Pathname(__FILE__).dirname
+Pathname.glob((SPEC_ROOT + 'fixtures/**/*.rb').to_s).each { |file| require file }
+Pathname.glob((SPEC_ROOT + 'integration/shared/**/*.rb').to_s).each { |file| require file }
+
+DataMapper::Spec.setup
+DataMapper.finalize
+
+Spec::Runner.configure do |config|
+  config.extend(DataMapper::Spec::Adapters::Helpers)
+
+  config.before :suite do
+    DataMapper.finalize.auto_migrate!
+  end
+end
