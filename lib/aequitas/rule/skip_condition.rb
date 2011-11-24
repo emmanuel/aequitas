a8d4b3a9b434f81aa2dffeaa93edca073d3c4ec9
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-11-24 13:08:26 -0800

    Add YARDoc for most of Rule::SkipCondition.

--- lib/aequitas/rule/skip_condition.rb
+++ lib/aequitas/rule/skip_condition.rb
@@ -18,6 +18,13 @@ module Aequitas
         @allow_blank = !!options[:allow_blank] if options.include?(:allow_blank)
       end
 
+      # Test a value to determine if it should be skipped,
+      #   according to the receiver's configuration
+      # 
+      # @param [#nil?] value
+      #   the value to be tested
+      # 
+      # @return [self]
       def skip?(value)
         if value.nil?
           @allow_nil.nil? ? allow_blank? : allow_nil?
@@ -28,36 +35,68 @@ module Aequitas
         end
       end
 
+      # Inquire whether the receiver is configured to allow nil values
+      # 
+      # @return [self]
       def allow_nil?
         @allow_nil.nil? ? false : @allow_nil
       end
 
+      # Inquire whether the receiver is configured to allow blank values
+      # 
+      # @return [self]
       def allow_blank?
         @allow_blank.nil? ? false : @allow_blank
       end
 
+      # Set the receiver to allow nil values
+      # 
+      # @return [self]
       def allow_nil!
         @allow_nil = true
+        self
       end
 
+      # Set the receiver to allow blank values
+      # 
+      # @return [self]
       def allow_blank!
         @allow_blank = true
+        self
       end
 
+      # Set the receiver to reject nil values
+      # 
+      # @return [self]
       def reject_nil!
         @allow_nil = false
+        self
       end
 
+      # Set the receiver to allow blank values
+      # 
+      # @return [self]
       def reject_blank!
         @allow_blank = false
+        self
       end
 
+      # Set the receiver to allow nil values
+      #   if a value has not already been set
+      # 
+      # @return [self]
       def default_to_allowing_nil!
-        allow_nil! if @allow_nil.nil?
+        allow_nil! if allow_nil.nil?
+        self
       end
 
+      # Set the receiver to allow blank values
+      #   if a value has not already been set
+      # 
+      # @return [self]
       def default_to_allowing_blank!
-        allow_blank! if @allow_blank.nil?
+        allow_blank! if allow_blank.nil?
+        self
       end
 
     end # class SkipCondition
