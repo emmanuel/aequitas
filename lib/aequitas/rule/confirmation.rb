de38d7049dfd695a8c6aee9fb6880760704955f4
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-11-24 13:11:47 -0800

    Fix inconsistent instance variable name in Rule::Confirmation.
    Settled on @confirmation_attribute_name.

--- lib/aequitas/rule/confirmation.rb
+++ lib/aequitas/rule/confirmation.rb
@@ -6,15 +6,15 @@ module Aequitas
   class Rule
     class Confirmation < Rule
 
-      equalize_on superclass.equalizer.keys + [:confirmation_attribute]
+      equalize_on superclass.equalizer.keys + [:confirmation_attribute_name]
 
-      attr_reader :confirmation_attribute
+      attr_reader :confirmation_attribute_name
 
 
       def initialize(attribute_name, options = {})
         super
 
-        @confirm_attribute_name = options.fetch(:confirm) do
+        @confirmation_attribute_name = options.fetch(:confirm) do
           :"#{attribute_name}_confirmation"
         end
 
@@ -29,7 +29,7 @@ module Aequitas
       end
 
       def confirmation_value(resource)
-        resource.instance_variable_get("@#{@confirm_attribute_name}")
+        resource.instance_variable_get("@#{@confirmation_attribute_name}")
       end
 
       def violation_type(resource)
