805d772346919aa9c14df7e635a3b872f0a635ed
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-12-02 00:41:28 -0800

    Add integration spec coverage of Aequitas::Rule::Numericalness subclasses.

--- spec/integration/aequitas/macros/validates_numericalness_of_spec.rb
+++ spec/integration/aequitas/macros/validates_numericalness_of_spec.rb
@@ -8,8 +8,6 @@ Aequitas::Macros::IntegrationSpec.describe Aequitas::Macros, '#validates_numeric
       class_under_test.validates_numericalness_of attribute_name
     end
 
-    let(:bound) { 1 }
-
     describe 'when validated attribute is a non-integer number' do
       let(:attribute_value) { 1.0 }
 
@@ -44,6 +42,180 @@ Aequitas::Macros::IntegrationSpec.describe Aequitas::Macros, '#validates_numeric
       let(:attribute_value) { 'a' }
 
       it_should_be_an_invalid_instance
+    end
+  end
+
+  describe 'with :eq, :equal, :equals, :exactly, or :equal_to options' do
+    before do
+      class_under_test.validates_numericalness_of attribute_name, :eq => bound
+    end
+
+    let(:bound) { 1 }
+    let(:class_of_violated_validation_rule) do
+      Aequitas::Rule::Numericalness::Equal
+    end
+
+    describe 'when validated attribute is greater than bound' do
+      let(:attribute_value) { bound + 1 }
+
+      it_should_be_an_invalid_instance
+    end
+
+    describe 'when validated attribute is equal to bound' do
+      let(:attribute_value) { bound }
+
+      it_should_be_a_valid_instance
+    end
+
+    describe 'when validated attribute is less than bound' do
+      let(:attribute_value) { bound - 1 }
+
+      it_should_be_an_invalid_instance
+    end
+  end
+
+  describe 'with :ne or :not_equal_to options' do
+    before do
+      class_under_test.validates_numericalness_of attribute_name, :ne => bound
+    end
+
+    let(:bound) { 1 }
+    let(:class_of_violated_validation_rule) do
+      Aequitas::Rule::Numericalness::NotEqual
+    end
+
+    describe 'when validated attribute is greater than bound' do
+      let(:attribute_value) { bound + 1 }
+
+      it_should_be_a_valid_instance
+    end
+
+    describe 'when validated attribute is equal to bound' do
+      let(:attribute_value) { bound }
+
+      it_should_be_an_invalid_instance
+    end
+
+    describe 'when validated attribute is less than bound' do
+      let(:attribute_value) { bound - 1 }
+
+      it_should_be_a_valid_instance
+    end
+  end
+
+  describe 'with :gt or :greater_than options' do
+    before do
+      class_under_test.validates_numericalness_of attribute_name, :gt => bound
+    end
+
+    let(:bound) { 1 }
+    let(:class_of_violated_validation_rule) do
+      Aequitas::Rule::Numericalness::GreaterThan
+    end
+
+    describe 'when validated attribute is greater than bound' do
+      let(:attribute_value) { bound + 1 }
+
+      it_should_be_a_valid_instance
+    end
+
+    describe 'when validated attribute is equal to bound' do
+      let(:attribute_value) { bound }
+
+      it_should_be_an_invalid_instance
+    end
+
+    describe 'when validated attribute is less than bound' do
+      let(:attribute_value) { bound - 1 }
+
+      it_should_be_an_invalid_instance
+    end
+  end
+
+  describe 'with :lt or :less_than options' do
+    before do
+      class_under_test.validates_numericalness_of attribute_name, :lt => bound
+    end
+
+    let(:bound) { 1 }
+    let(:class_of_violated_validation_rule) do
+      Aequitas::Rule::Numericalness::LessThan
+    end
+
+    describe 'when validated attribute is greater than bound' do
+      let(:attribute_value) { bound + 1 }
+
+      it_should_be_an_invalid_instance
+    end
+
+    describe 'when validated attribute is equal to bound' do
+      let(:attribute_value) { bound }
+
+      it_should_be_an_invalid_instance
+    end
+
+    describe 'when validated attribute is less than bound' do
+      let(:attribute_value) { bound - 1 }
+
+      it_should_be_a_valid_instance
+    end
+  end
+
+  describe 'with :gte or :greater_than_or_equal_to options' do
+    before do
+      class_under_test.validates_numericalness_of attribute_name, :gte => bound
+    end
+
+    let(:bound) { 1 }
+    let(:class_of_violated_validation_rule) do
+      Aequitas::Rule::Numericalness::GreaterThanOrEqual
+    end
+
+    describe 'when validated attribute is greater than bound' do
+      let(:attribute_value) { bound + 1 }
+
+      it_should_be_a_valid_instance
+    end
+
+    describe 'when validated attribute is equal to bound' do
+      let(:attribute_value) { bound }
+
+      it_should_be_a_valid_instance
+    end
+
+    describe 'when validated attribute is less than bound' do
+      let(:attribute_value) { bound - 1 }
+
+      it_should_be_an_invalid_instance
+    end
+  end
+
+  describe 'with :lte or :less_than_or_equal_to options' do
+    before do
+      class_under_test.validates_numericalness_of attribute_name, :lte => bound
+    end
+
+    let(:bound) { 1 }
+    let(:class_of_violated_validation_rule) do
+      Aequitas::Rule::Numericalness::LessThanOrEqual
+    end
+
+    describe 'when validated attribute is greater than bound' do
+      let(:attribute_value) { bound + 1 }
+
+      it_should_be_an_invalid_instance
+    end
+
+    describe 'when validated attribute is equal to bound' do
+      let(:attribute_value) { bound }
+
+      it_should_be_a_valid_instance
+    end
+
+    describe 'when validated attribute is less than bound' do
+      let(:attribute_value) { bound - 1 }
+
+      it_should_be_a_valid_instance
     end
   end
 end
