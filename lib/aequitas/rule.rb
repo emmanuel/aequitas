438b1c96d12d67da47d6cb858ebbea98ec470404
Markus Schirp <mbj@seonic.net>
2012-10-15 03:21:45 +0200

    Revert "Cleanup Aequitas::Rule documentation"
    This reverts commit 4e75b455a231f7716a33a61f1302fde481743b11.

--- lib/aequitas/rule.rb
+++ lib/aequitas/rule.rb
@@ -48,8 +48,6 @@ module Aequitas
     # 
     # @return [#each(Rule)]
     #   a collection of validators which collectively
-    #
-    # @api private
     # 
     def self.rules_for(attribute_name, options, &block)
       Array(new(attribute_name, options, &block))
@@ -67,18 +65,15 @@ module Aequitas
     # @option [String, Hash] :message
     #   A custom message that will be used for any violations of this rule
     # @option [Symbol, Proc] :if
-    #   The name of a method (on the valiated context) or a Proc to call
-    #   (with the context) to determine if the rule should be applied.
+    #   The name of a method (on the valiated resource) or a Proc to call
+    #   (with the resource) to determine if the rule should be applied.
     # @option [Symbol, Proc] :unless
-    #   The name of a method (on the valiated context) or a Proc to call
-    #   (with the context) to determine if the rule should *not* be applied.
+    #   The name of a method (on the valiated resource) or a Proc to call
+    #   (with the resource) to determine if the rule should *not* be applied.
     # @option [Boolean] :allow_nil
     #   Whether to skip applying this rule on nil values
     # @option [Boolean] :allow_blank
     #   Whether to skip applying this rule on blank values
-    #
-    # @api private
-    #
     def initialize(attribute_name, options = {})
       @attribute_name = attribute_name
       @custom_message = options.fetch(:message, nil)
@@ -86,41 +81,41 @@ module Aequitas
       @skip_condition = options.fetch(:skip_condition) { SkipCondition.new(options) }
     end
 
-    # Validate the +context+ arg against this Rule
+    # Validate the +resource+ arg against this Rule
     # 
-    # @param [Object] context
+    # @param [Object] resource
     #   the target object to be validated
     # 
     # @return [nil]
-    #   if +context+ is valid
+    #   if +resource+ is valid
     #
     # @return [Violation]
     #   otherwise
     #
     # @api private
     #
-    def validate(context)
-      value = attribute_value(context)
+    def validate(resource)
+      value = attribute_value(resource)
 
       if skip?(value) || valid_value?(value)
         nil
       else
-        new_violation(context, value)
+        new_violation(resource, value)
       end
     end
 
-    # Test if rule should run on context
+    # Test if rule should run on resource
     #
     # @return [true]
-    #   if rule should be executed on context
+    #   if rule should be executed on resource
     #
     # @return [false]
     #   otherwise
     #
     # @api private
     #
-    def execute?(context)
-      guard.allow?(context)
+    def execute?(resource)
+      guard.allow?(resource)
     end
 
     # Test if rule is skipped on value
@@ -142,48 +137,42 @@ module Aequitas
 
     # Return attribute value to execute on rule
     #
-    # @param [Object] context
+    # @param [Object] resource
     #
     # @return [Object]
     #
     # @api private
     # 
-    def attribute_value(context)
-      context.validation_attribute_value(attribute_name)
+    def attribute_value(resource)
+      resource.validation_attribute_value(attribute_name)
     end
 
-    private
-
-    # Return new violation
-    #
-    # @param [Object] context
-    # @param [Object] value
-    #
     # @api private
-    #
-    def new_violation(context, value = nil)
-      Violation::Rule.new(context, custom_message,
+    def violation_info
+      Hash[ violation_data ]
+    end
+
+    # @api private
+    def violation_values
+      violation_data.map { |(_, value)| value }
+    end
+
+    # @api private
+    def violation_data
+      [ ]
+    end
+
+  private
+
+    def new_violation(resource, value = nil)
+      Violation::Rule.new(resource, custom_message,
         :rule  => self,
         :value => value)
     end
 
-    # Assert value is kind of klasses
-    #
-    # @param [Symbol] name
-    # @param [Object] value
-    # @param [Enumerable<Class>] *klasses
-    #
-    # @return [self]
-    #
-    # @raise [ArgumentError]
-    #   if value is not kind of klasses
-    #
-    # @api private
-    #
     def assert_kind_of(name, value, *klasses)
-      klasses.each { |klass| return if value.kind_of?(klass) }
-      raise ArgumentError, "+#{name}+ should be #{klasses.map { |klass| klass.name } * ' or '}, but was #{value.class.name}", caller(2)
-      self
+      klasses.each { |k| return if value.kind_of?(k) }
+      raise ArgumentError, "+#{name}+ should be #{klasses.map { |k| k.name } * ' or '}, but was #{value.class.name}", caller(2)
     end
 
   end # class Rule
